5d93eb373f6a41ecaf3ba4c6be50c1f6d0d9319f
nataliegarate <natalieegarate@hotmail.com>
2020-05-15 14:29:45 -0700

    added lists dir

---
+++ lists/remove_evens.py
@@ -0,0 +1,17 @@
+def removeEven(List):
+    num_of_evens = 0
+    idx = 0
+    for num in List:
+        if num % 2 == 0:
+            num_of_evens += 1
+        else:
+            List[idx - num_of_evens] = num
+        idx += 1
+    count = 0
+    while count < num_of_evens:
+        List.pop()
+        count += 1
+    return List
+
+
+print(removeEven([1, 2, 3, 4]))
